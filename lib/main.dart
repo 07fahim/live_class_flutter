05cf01fe72e51e7114571268cf7317a4fa0b5370
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-10-07 00:07:41 +0600

    image widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,65 @@ class MyApp extends StatelessWidget {
         //   fit: BoxFit.scaleDown,
         //   alignment: Alignment.bottomCenter,
         // )
-        body: Image.network(
-            'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-            width: 350, height: 400,
-        fit: BoxFit.scaleDown,alignment: Alignment.bottomCenter),
-      ),
+        //   body: Image.network(
+        //       'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
+        //       width: 350, height: 400,
+        //   fit: BoxFit.scaleDown,alignment: Alignment.bottomCenter),
+        // )
+        //   body: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     mainAxisSize: MainAxisSize.min,
+        //     children: [
+        //       Text("Hello"),
+        //       Text("Fahim Faiyaz"),
+        //       Image.network(
+        //           'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
+        //           width: 350,
+        //           height: 400,
+        //           fit: BoxFit.cover),
+        //       Text("CR7")
+        //     ],
+        //   ),
+        // body: Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        //     Text('Hello'),
+        //     Text("Fahim Faiyaz"),
+        // Image.network(
+        //             'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
+        //             width: 200,
+        //             fit: BoxFit.cover),
+        //         Text("CR7")
+        //   ],
+        // ),
+          body: Column(
+            children: [
+              Text("Hello"),
+              Text("Fahim Faiyaz"),
+              Row(
+                children: [
+                  Image.network(
+                      'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
+                      width: 200,
+                      fit: BoxFit.cover),
+                  Text("CR7")
+                ],
+              ),
+              Row(
+                children: [
+                  Image.network(
+                                'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
+                                width: 200,
+                                fit: BoxFit.cover),
+                            Text("CR7")
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 }
