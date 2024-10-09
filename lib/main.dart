c8f263fdbec310f624adb605aaf8db0c92171641
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-10-09 21:44:53 +0600

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -9,79 +9,137 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home"),
-          centerTitle: true,
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home App",
+          style: TextStyle(
+              fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        // body: Image.asset(
-        //   'assets/images/cr7.jpg',
-        //   width: 350,
-        //   height: 400,
-        //   fit: BoxFit.scaleDown,
-        //   alignment: Alignment.bottomCenter,
-        // )
-        //   body: Image.network(
-        //       'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-        //       width: 350, height: 400,
-        //   fit: BoxFit.scaleDown,alignment: Alignment.bottomCenter),
-        // )
-        //   body: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     crossAxisAlignment: CrossAxisAlignment.center,
-        //     mainAxisSize: MainAxisSize.min,
-        //     children: [
-        //       Text("Hello"),
-        //       Text("Fahim Faiyaz"),
-        //       Image.network(
-        //           'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-        //           width: 350,
-        //           height: 400,
-        //           fit: BoxFit.cover),
-        //       Text("CR7")
-        //     ],
-        //   ),
-        // body: Row(
-        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   mainAxisSize: MainAxisSize.min,
-        //   children: [
-        //     Text('Hello'),
-        //     Text("Fahim Faiyaz"),
-        // Image.network(
-        //             'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-        //             width: 200,
-        //             fit: BoxFit.cover),
-        //         Text("CR7")
-        //   ],
-        // ),
-          body: Column(
-            children: [
-              Text("Hello"),
-              Text("Fahim Faiyaz"),
-              Row(
-                children: [
-                  Image.network(
-                      'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-                      width: 200,
-                      fit: BoxFit.cover),
-                  Text("CR7")
-                ],
+        titleSpacing: 2,
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+        elevation: 100,
+        toolbarHeight: 80,
+        toolbarOpacity: 1,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Taped on elevated button');
+                },
+                child: Text("Tap"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  foregroundColor: Colors.white,
+                  alignment: Alignment.center,
+                  elevation: 10,
+                  shadowColor: Colors.purple,
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: Colors.white,
+                          width: 4,
+                          strokeAlign: BorderSide.strokeAlignOutside),
+                      borderRadius: BorderRadius.circular(10)),
+                  minimumSize: Size(150, 50),
+                )),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                print("Click on");
+              },
+              child: Text("Click"),
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.pink,
+                  foregroundColor: Colors.white,
+                  alignment: Alignment.center,
+                  elevation: 10,
+                  shadowColor: Colors.purple,
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: Colors.white,
+                          width: 4,
+                          strokeAlign: BorderSide.strokeAlignOutside),
+                      borderRadius: BorderRadius.circular(10)),
+                  minimumSize: Size(150, 50)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.insert_emoticon_sharp),
+                style: IconButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                    foregroundColor: Colors.white,
+                    alignment: Alignment.center,
+                    elevation: 10,
+                    shadowColor: Colors.purple,
+                    padding: EdgeInsets.all(30),
+                    iconSize: 30,
+                    shape: CircleBorder(
+                        side: BorderSide(
+                            color: Colors.white,
+                            width: 4,
+                            strokeAlign: BorderSide.strokeAlignOutside)),
+                    fixedSize: Size(100, 100))),
+            SizedBox(height: 20),
+            FloatingActionButton.large(backgroundColor: Colors.pink,
+              foregroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(Icons.add),
+              tooltip: 'This is a add button',
+              elevation: 10,
+              shape: CircleBorder(side: BorderSide(color: Colors.white,width: 4,strokeAlign: BorderSide.strokeAlignOutside)),
+
+            ),
+            SizedBox(height: 20,),
+            OutlinedButton(onPressed: (){}, child:Text("Click Here"),style:OutlinedButton.styleFrom(backgroundColor: Colors.pink,
+              foregroundColor: Colors.white,
+              alignment: Alignment.center,
+              elevation: 10,
+              shadowColor: Colors.purple,
+              textStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              Row(
-                children: [
-                  Image.network(
-                                'https://wallpapers.com/images/hd/cristiano-ronaldo-chest-signal-5j34l030l0ys94wg.jpg',
-                                width: 200,
-                                fit: BoxFit.cover),
-                            Text("CR7")
-                ],
-              )
-            ],
-          ),
-        )
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                      color: Colors.white,
+                      width: 4,
+                      strokeAlign: BorderSide.strokeAlignOutside),
+                  borderRadius: BorderRadius.circular(10)),
+              minimumSize: Size(150, 50),
+            ),)
+          ],
+        ),
+      ),
     );
   }
 }
