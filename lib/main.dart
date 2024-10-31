c183bd1b2b4d1ce68d3b4e85dd67bd04204a1dc9
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-10-31 19:41:04 +0600

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -42,31 +42,86 @@ class Home extends StatelessWidget {
         title: Text('Home'),
     backgroundColor: Colors.green,
     ),
-      body: ListView.separated(
-          itemCount:friendList.length,
-          itemBuilder:(BuildContext context,int index){
-            return Column(
-              children: [
-                Text(friendList[index]),
-                // Divider(
-                //   height: 10,
-                //   thickness: 5,
-                //   color: Colors.redAccent,
-                //   indent: 10,
-                //   endIndent:12,
-                // )
-              ],
-            );
-          },
-          separatorBuilder:(BuildContext context,int index){
-            return Divider(
-              height: 10,
-              thickness: 10,
-              endIndent: 10,
-            );
-          }
+//       body: ListView.separated(
+//           itemCount:friendList.length,
+//           itemBuilder:(BuildContext context,int index){
+//             return Column(
+//               children: [
+//                 Text(friendList[index]),
+//                 // Divider(
+//                 //   height: 10,
+//                 //   thickness: 5,
+//                 //   color: Colors.redAccent,
+//                 //   indent: 10,
+//                 //   endIndent:12,
+//                 // )
+//               ],
+//             );
+//           },
+//           separatorBuilder:(BuildContext context,int index){
+//             return Divider(
+//               height: 10,
+//               thickness: 10,
+//               endIndent: 10,
+//             );
+//           }
+//           ),
+//
+//     );
+//   }
+// }
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.only(left: 24),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.pink.withOpacity(0.4),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ]
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      border: Border.all(color: Colors.black, width: 2),
+                      // borderRadius: BorderRadius.circular(16)
+                      // borderRadius: BorderRadius.only(
+                      //   topLeft: Radius.circular(16),
+                      //   bottomRight: Radius.circular(16)
+                      // ),
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/cr7.jpg'),
+                        fit: BoxFit.cover,
+                        opacity: 0.3,
+                      ),
+                    ),
+                    child: Text('Random'),
+                  ),
+                  Text('Shoe')
+                ],
+              ),
+            ],
           ),
-
+        ],
+      ),
     );
   }
 }
