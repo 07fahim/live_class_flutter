22ddf552624b1d3c1e3a7082714917a543425fc2
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-11-11 21:28:40 +0600

    Cupertino Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,214 +1,70 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-// ListView.Separator, Container
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HelloWorldApp());
+  runApp(CupertinoExpApp());
 }
 
-class HelloWorldApp extends StatelessWidget {
+class CupertinoExpApp extends StatelessWidget {
+  const CupertinoExpApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello World App',
+    if(Platform.isIOS){
+    return const CupertinoApp(
       home: Home(),
-      theme: ThemeData(
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.redAccent,
-            foregroundColor: Colors.white,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          )),
-          appBarTheme: AppBarTheme(
-              backgroundColor: Colors.purple,
-              foregroundColor: Colors.white,
-              titleTextStyle:
-                  TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              borderSide: BorderSide(color: Colors.purple),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              borderSide: BorderSide(color: Colors.pink),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              borderSide: BorderSide(color: Colors.blue),
-            ),
-          )),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.redAccent,
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        )),
-        appBarTheme: AppBarTheme(
-            backgroundColor: Colors.purple,
-            foregroundColor: Colors.white,
-            titleTextStyle:
-                TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            borderSide: BorderSide(color: Colors.purple),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            borderSide: BorderSide(color: Colors.pink),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            borderSide: BorderSide(color: Colors.blue),
-          ),
-        ),
-      ),
-      themeMode: ThemeMode.system,
-    );
+      debugShowCheckedModeBanner: false,
+    );}
+    else{
+      return MaterialApp(
+        home: Home(),
+      );
+    }
   }
 }
 
 class Home extends StatelessWidget {
-  Home({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        // backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(),
-              TextField(),
-              TextField(),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Profile(
-                                  username: 'Fahim',
-                                )));
-                  },
-                  child: Text("Go To Profile")),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Settings()));
-                  },
-                  child: Text("Go To Settings"))
-            ],
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          padding: EdgeInsetsDirectional.all(10),
+          middle: Text("Home"),
+          backgroundColor: CupertinoColors.link,
+          leading: Icon(
+            CupertinoIcons.home,
+            color: CupertinoColors.white,
+          ),
+          trailing: Icon(
+            CupertinoIcons.add_circled,
+            color: CupertinoColors.white,
           ),
         ),
-      ),
-    );
-  }
-}
-
-class Profile extends StatelessWidget {
-  const Profile({super.key, required this.username});
-
-  final String username;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-      ),
-      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(username),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Back")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => Settings()));
-                },
-                child: Text("Move to Settings")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => Home()),
-                      (predicate) => false);
-                },
-                child: Text('Back to Home')),
+            Center(
+                child: CupertinoButton.filled(
+              borderRadius: BorderRadius.circular(10),
+              child: const Text(
+                "Tap Here",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+            )),
+            CupertinoListTile(
+              title: Text("Name"),
+              subtitle: Text("name"),
+              trailing: Icon(CupertinoIcons.airplane),
+            ),
+            CupertinoTextField(),
+            CupertinoSwitch(value:false, onChanged:(value){})
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class Settings extends StatelessWidget {
-  const Settings({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Profile(
-                                username: "Faiyaz",
-                              )));
-                },
-                child: Text("Move to Profile")),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-              Container(
-                height:300,
-                width: 300,
-                color: Colors.blue,
-              ),
-              Container(
-                height:200,
-                width: 200,
-                color: Colors.red,
-              ),
-              Positioned(
-                bottom: 10,
-                top: 20,
-                left: 30,
-
-                child: Container(
-                  height:100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
-              ),
-            ],)
-          ],
-        ),
-      ),
-    );
+        ));
   }
 }
